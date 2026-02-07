c2cc5d8196710c7dae18c05e5f0f89e04a9ec909
kevinskt8-creator <kevinskt8@gmail.com>
2026-02-07 20:34:47 -0300

    Variáveis Const e Let

---
+++ Fundamentos/comentario.js
@@ -0,0 +1,15 @@
+//Comentário de uma linha
+console.log('Linha 1')
+
+/* Comentário de 
+múltiplas linhas
+*/
+
+/*
+* Comentário de 
+* múltiplas linhas
+* Clean code
+* Sem Comentários Desnecessários
+*/
+
+console.log('Linha 3')
