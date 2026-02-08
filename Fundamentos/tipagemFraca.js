069ae3013445e533a41c8e87d2da83bd469e2db0
kevinskt8-creator <kevinskt8@gmail.com>
2026-02-07 21:01:43 -0300

    Tipo de dado Number

---
+++ Fundamentos/tipagemFraca.js
@@ -0,0 +1,14 @@
+let qualquer = 'Legal'
+console.log(qualquer)
+console.log(typeof qualquer)   
+
+// A mesma variável pode ter múltiplo valores, o que significa que Javascript é uma liguagem de tipagem fraca
+
+qualquer = 3.1516
+console.log(qualquer)
+console.log(typeof qualquer)
+
+// Evitar nome genéricos e siglas
+let valor = ''
+let numero = 1
+let pqp = false // Produto Químico Perigoso... kkkk
