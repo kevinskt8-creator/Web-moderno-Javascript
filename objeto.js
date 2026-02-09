b5c97c866987f42acfba2d7c26578d2a9812aa25
kevinskt8-creator <kevinskt8@gmail.com>
2026-02-09 20:44:39 -0300

    objeto

---
+++ objeto.js
@@ -0,0 +1,15 @@
+const prod1 = {};
+prod1.nome = "Celular Ultra mega";
+prod1.preco = 4998.9;
+prod1["Desconto Legal"] = 0.4; // evitar atributos com espaço
+
+//Obejto : Coleção de chave e valor {} e valor
+
+console.log(prod1);
+
+const prod2 = {
+  nome: "Camisa Polo",
+  preco: 79.9,
+}
+
+console.log(prod2);
