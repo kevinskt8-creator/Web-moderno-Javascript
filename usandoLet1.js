7a863ee751c3b7b88460289051262a3cfd194748
kevinskt8-creator <kevinskt8@gmail.com>
2026-02-10 19:14:51 -0300

    Usando Let e Var

---
+++ usandoLet1.js
@@ -0,0 +1,10 @@
+var numero = 1
+{
+    let numero = 2
+    console.log('dentro =', numero)
+}
+
+    console.log('fora =', numero)
+// Let: Embora a variavel tenha o mesmo nome, estão em escopos diferentes portando poderam cooexistir.
+// Variáveis definadas com a palavra 'Var' tem escopo global, e tem escopo de função
+// Let: escopo global, escopo de função e escopo de bloco.
