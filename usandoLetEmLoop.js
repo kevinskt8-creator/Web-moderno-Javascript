a16078f54d67736addd4ac26f185ecb344718fad
kevinskt8-creator <kevinskt8@gmail.com>
2026-02-10 19:41:16 -0300

    Var e let em Loop 002

--- usandoLetEmLoop.js
+++ usandoLetEmLoop.js
@@ -3,3 +3,20 @@ for (let i = 0; i < 20; i++) {
 }
 
 console.log('i =', 1)
+
+
+//usandoLetEmLoop2
+
+
+const funcs = []
+
+for (let i = 0; i < 10; i++) {
+    funcs.push(function() {
+        console.log(i)
+
+    })
+}
+
+funcs[2]()
+funcs[8]()
+funcs[6]()
