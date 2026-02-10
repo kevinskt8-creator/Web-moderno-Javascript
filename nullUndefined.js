1acae1f46341a77ce7b8141b16991d92e57ae7ed
kevinskt8-creator <kevinskt8@gmail.com>
2026-02-09 21:18:03 -0300

    Unfefined e Null

---
+++ nullUndefined.js
@@ -0,0 +1,23 @@
+let vlaor;
+// não inicializada
+console.log(vlaor);
+
+valor = null; //ausência de valor
+console.log(valor);
+// console.log(valor.toString()) // Erro!
+
+const produto = {};
+console.log(produto.preco);
+console.log(produto);
+
+produto.preco = 3.5;
+console.log(produto);
+
+produto.preco = undefined; // evitee atribuir undefined
+console.log(!!produto.preco);
+// delete produto.preco , forma de tirar um atributo
+console.log(produto);
+
+produto.preco = null; //sem preço
+console.log(!!produto.preco);
+console.log(produto);
