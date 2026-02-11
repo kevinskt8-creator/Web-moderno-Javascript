8be0948ac54dd27bbbd20ade395d4d6e245ea8a0
kevinskt8-creator <kevinskt8@gmail.com>
2026-02-11 15:57:44 -0300

    Destructuring 001

--- destructuring.js
+++ destructuring.js
@@ -5,8 +5,20 @@ const pessoa = {
   idade: 5,
   endereco: {
     logradouro: "Rua ABC",
-    numero: 1000
-  }
+    numero: 1000,
+  },
 };
 
-const { }
+const { nome, idade } = pessoa;
+console.log(nome, idade);
+
+const { nome: n, idade: i } = pessoa;
+console.log(n, i)
+
+const { sobrenome, bemHumorada = true} = pessoa
+console.log(sobrenome, bemHumorada)
+
+const { endereco: { logradouro, numero, cep, }  } = pessoa
+console.log(logradouro, numero, cep)
+
+
