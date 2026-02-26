5f23ead559a8b89229a0f7b50bb3df75e3cc355d
kevinskt8-creator <kevinskt8@gmail.com>
2026-02-26 17:37:45 -0300

    while

---
+++ Estrutura de controle/while.js
@@ -0,0 +1,14 @@
+function genteInteiroAleatorioEntre(min, max) {
+  const valor = Math.random() * (max - min) + min
+  return Math.floor(valor)
+
+}
+
+let opcao = 0
+
+while (opcao != -1) {
+    opcao = genteInteiroAleatorioEntre(-1, 10)
+    console.log(`Opção escolhida foi' ${opcao}.`)
+}
+
+console.log('Até a próxima')
