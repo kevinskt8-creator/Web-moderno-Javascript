af3f16ac8d1a63ed349cb4406971224e8f830948
kevinskt8-creator <kevinskt8@gmail.com>
2026-03-01 13:09:20 -0300

    Callback 01

--- Funções/callback1.js
+++ Funções/callback1.js
@@ -1,1 +1,8 @@
-const fabricantes = [";Mercedes",]
+const fabricantes = ["Mercedes", "Audi", "BMW"];
+
+function imprimir(nome, indice) {
+  console.log(`${(indice = 1)}. ${nome}`);
+}
+
+fabricantes.forEach(imprimir);
+fabricantes.forEach((fabricante) => console.log(fabricante));
