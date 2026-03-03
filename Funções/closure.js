2148bb916febfd98d7e2b5bdaeea5f9ac5ebb1c5
kevinskt8-creator <kevinskt8@gmail.com>
2026-03-03 19:28:02 -0300

    update closure

--- Funções/closure.js
+++ Funções/closure.js
@@ -1,1 +1,17 @@
-// Closure é o escopo criando quando uma função é declarada
+// Closure é o escopo criando quando uma função é declarada/ escopo que envolve a função
+// Esse escopo permite a funçaõ acessar e manipular variáveis externas à função= 'Local
+
+// Contexto léxico em ação!
+
+const x = "Global";
+
+function fora() {
+  const x = "Local";
+  function dentro() {
+    return x;
+  }
+  return dentro;
+}
+
+const minhaFuncao = fora();
+console.log(minhaFuncao())
