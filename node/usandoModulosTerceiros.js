37f87bc8513bc093ae959137c1f1fa2b5b5ef383
kevinskt8-creator <kevinskt8@gmail.com>
2026-03-27 14:47:14 -0300

    require

--- node/usandoModulosTerceiros.js
+++ node/usandoModulosTerceiros.js
@@ -1,2 +1,16 @@
-const _ = require('lodash')
-setInterval(() => console.log(_.random(1, 1000)), 2000)
+const url = 'http://files.cod3r.com.br/curso-js/funcionarios.json'
+const axios = require('axios')
+
+axios.get(url).then(response => {
+    const funcionarios = response.data
+
+    const mulheresChinesas = funcionarios
+        .filter(f => f.genero === 'F')
+        .filter(f => f.pais === 'China')
+
+    const mulherChinesaMenorSalario = mulheresChinesas.reduce((menor, atual) => {
+        return atual.salario < menor.salario ? atual : menor
+    })
+
+    console.log(mulherChinesaMenorSalario)
+})
