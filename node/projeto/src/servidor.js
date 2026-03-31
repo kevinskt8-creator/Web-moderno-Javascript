1bb3ebddd1fe1b7bbe984cef3050ccd1636163db
kevinskt8-creator <kevinskt8@gmail.com>
2026-03-31 20:14:24 -0300

    API rest com Express

---
+++ node/projeto/src/servidor.js
@@ -0,0 +1,17 @@
+const porta = 3003;
+
+const express = require("express");
+const app = express();
+
+app.get('/produtos', (req, res, nexxt) => {
+    console.log('Middleware 1...')
+    next()
+})
+
+app.get("/produtos", (req, res, next) => {
+  res.send({ nome: "Notebook", preco: 123.45 }); // converter para JSON
+});
+
+app.listen(porta, () => {
+  console.log(`Servidor executando na porta ${porta}.`);
+});
