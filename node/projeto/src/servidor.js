027d3c527beaf245329dc16737d3830468a9a347
kevinskt8-creator <kevinskt8@gmail.com>
2026-04-03 18:49:24 -0300

    ok

--- node/projeto/src/servidor.js
+++ node/projeto/src/servidor.js
@@ -2,24 +2,26 @@ const porta = 3003;
 
 const express = require("express");
 const app = express();
-const bancoDeDados = require('./bancoDeDados')
+const bodyParser = require("body-parser");
+const bancoDeDados = require("./bancoDeDados");
 
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/produtos", (req, res, next) => {
-res.send(bancoDeDados.getProdutos())
+  res.send(bancoDeDados.getProdutos());
 });
 
-app.get('/produtos/:id', (req, res, next) => {
-  res.send(bancoDeDados.getProduto(req.params.id))
+app.get("/produtos/:id", (req, res, next) => {
+  res.send(bancoDeDados.getProduto(req.params.id));
+});
 
-})
-
-app.post('/produtos', (req, res, next) => {
+app.post("/produtos", (req, res, next) => {
   const produto = bancoDeDados.salvarProduto({
-    nome: req.body.name,
-    preco: req.body.preco
-  })
-})
+    nome: req.body.nome,
+    preco: req.body.preco,
+  });
+  res.send(produto); //JSON
+});
 
 app.listen(porta, () => {
   console.log(`Servidor executando na porta ${porta}.`);
