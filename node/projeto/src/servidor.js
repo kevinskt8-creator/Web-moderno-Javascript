3bad390fa0b54ae506069d6addeadce71098bf24
kevinskt8-creator <kevinskt8@gmail.com>
2026-04-03 18:34:39 -0300

    api express 03

--- node/projeto/src/servidor.js
+++ node/projeto/src/servidor.js
@@ -2,15 +2,24 @@ const porta = 3003;
 
 const express = require("express");
 const app = express();
+const bancoDeDados = require('./bancoDeDados')
 
-app.get('/produtos', (req, res, nexxt) => {
-    console.log('Middleware 1...')
-    next()
-})
 
 app.get("/produtos", (req, res, next) => {
-  res.send({ nome: "Notebook", preco: 123.45 }); // converter para JSON
+res.send(bancoDeDados.getProdutos())
 });
+
+app.get('/produtos/:id', (req, res, next) => {
+  res.send(bancoDeDados.getProduto(req.params.id))
+
+})
+
+app.post('/produtos', (req, res, next) => {
+  const produto = bancoDeDados.salvarProduto({
+    nome: req.body.name,
+    preco: req.body.preco
+  })
+})
 
 app.listen(porta, () => {
   console.log(`Servidor executando na porta ${porta}.`);
