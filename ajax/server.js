5b115aaa0e9301f69e6ba01649ae12d881513569
kevinskt8-creator <kevinskt8@gmail.com>
2026-04-24 16:10:48 -0300

    Axios

--- ajax/server.js
+++ ajax/server.js
@@ -38,5 +38,12 @@ app.post('/upload', (req, res) => {
         })
     })
 
+    app.get('/parOuImpar', (req, res) => {
+        const par = parseInt(req.query.numero) % 2 === 0 
+        res.send({
+            resultado: par ? 'par' : 'impar'
+        })
+    })
+
 
 app.listen(8081, () => console.log("Executnado..."));
