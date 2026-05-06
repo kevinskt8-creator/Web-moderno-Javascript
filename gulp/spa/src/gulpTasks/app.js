d2bbd21a2119dcc88ba2a436ce415008def438b5
kevinskt8-creator <kevinskt8@gmail.com>
2026-05-06 15:06:04 -0300

    Update app.js

--- gulp/spa/src/gulpTasks/app.js
+++ gulp/spa/src/gulpTasks/app.js
@@ -1,43 +1,36 @@
-const gulp = require('gulp')
-const babel = require('gulp-babel')
-const uglify = require('gulp-uglify')
-const sass = require('gulp-sass')(require('sass'))
-const uglifycss = require('gulp-uglifycss')
-const concat = require('gulp-concat')
-const htmlmin = require('gulp-htmlmin')
+const gulp = require("gulp");
+const babel = require("gulp-babel");
+const uglify = require("gulp-uglify");
+const sass = require("gulp-sass");
+const uglifycss = require("gulp-uglifycss");
+const concat = require("gulp-concat");
+const htmlmin = require("gulp-htmlmin");
 
 function appHtml() {
-    return gulp.src('src/**/*.html')
-        .pipe(htmlmin({ collapseWhitespace: true }))
-        .pipe(gulp.dest('build'))
+  return gulp
+    .src("src/**/*.html")
+    .pipe(htmlmin({ collapseWhitespace: true }))
+    .pipe(gulp.dest("build"));
 }
 
 function appCSS() {
-    return gulp.src('src/assets/sass/index.scss')
-        .pipe(sass().on('error', sass.logError))
-        .pipe(uglifycss({ uglyComments: false }))
-        .pipe(concat('app.min.css'))
-        .pipe(gulp.dest('build/assets/css'))
+  return gulp.src("src/assets/sass/index.scss");
+  .pipe(sass().on('error', sass.logError))
+  .pipe(uglifycss({ "uglifyComments": true }))
+  .pipe(concat())
 }
 
-function appJS() {
-    return gulp.src('src/assets/js/**/*.js')
-        .pipe(babel({
-            presets: ['env']
-        }))
-        .pipe(uglify())
-        .pipe(concat('app.min.js'))
-        .pipe(gulp.dest('build/assets/js'))
+function appJS(cb) {
+  return cb();
 }
 
-function appIMG() {
-    return gulp.src('src/assets/imgs/**/*.*')
-        .pipe(gulp.dest('build/assets/imgs'))
+function appIMG(cb) {
+  return cb();
 }
 
 module.exports = {
-    appHtml,
-    appCSS,
-    appJS,
-    appIMG
-}
+  appHtml,
+  appCSS,
+  appJS,
+  appIMG,
+};
