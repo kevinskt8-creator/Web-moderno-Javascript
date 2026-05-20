5db945efe496a50b316d0673d6bf8f868165673f
kevinskt8-creator <kevinskt8@gmail.com>
2026-05-20 20:07:49 -0300

    Fim WebPack

--- WebPack/src/assets/index.js
+++ WebPack/src/assets/index.js
@@ -1,2 +1,2 @@
 import './css/estilo.css'
-import '.scss/index.scss'
+import './scss/index.scss'
--- WebPack/webpack.config.js
+++ WebPack/webpack.config.js
@@ -1,21 +1,51 @@
-const webpack = require("webpack");
+const path = require('path')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin')
 
-module.exports = {
-  mode: "development",
-  entry: "./src/principal.js",
-  output: {
-    filename: "principal.js",
-    path: __dirname + "/public"
-  },
-  module: {
-    rules: [
-      {
-        test: /\.css$/,
-        use: [
-          "style-loader", // Adiciona CSS a DOM injetando a tag <style>
-          "css-loader", // interpreta @import, url()...
-        ],
-      },
+module.exports = (env, argv) => {
+  const modoDev = argv.mode !== 'production'
+
+  return {
+    mode: modoDev ? 'development' : 'production',
+    entry: "./src/principal.js",
+    output: {
+      filename: "principal.js",
+      path: path.resolve(__dirname, "public"),
+      assetModuleFilename: "imgs/[name][ext]"
+    },
+    devServer: {
+      contentBase: "./public",
+      port: 9000
+    },
+    optimization: {
+      minimizer: [
+        "...",
+        new CssMinimizerPlugin()
+      ]
+    },
+    plugins: [
+      new MiniCssExtractPlugin({
+        filename: "estilo.css"
+      })
     ],
-  },
-};
+    resolve: {
+      extensions: [".js", ".scss", ".css"]
+    },
+    module: {
+      rules: [
+        {
+          test: /\.s?[ac]ss$/,
+          use: [
+            modoDev ? "style-loader" : MiniCssExtractPlugin.loader,
+            "css-loader", // interpreta @import, url()...
+            "sass-loader"
+          ],
+        },
+        {
+          test: /\.(png|jpe?g|gif|svg)$/i,
+          type: "asset/resource"
+        },
+      ],
+    },
+  }
+}
