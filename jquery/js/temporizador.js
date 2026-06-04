81e92fc08559d24e7943e0aa4557b0d175819c6d
kevinskt8-creator <kevinskt8@gmail.com>
2026-06-04 15:52:39 -0300

    jquery 3

---
+++ jquery/js/temporizador.js
@@ -0,0 +1,10 @@
+(function ($) {
+    $.fn.temporizador = function (opcoes) {
+        const opcoesfinais = $.extend({
+            mensagem: 'Em Breve!',
+            horario: '23:59:59'
+        }, opcoes)
+        
+        return this 
+    }
+})
