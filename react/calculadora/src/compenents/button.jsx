1da871f31292ae983ee6e1ce3285f2b331f580e9
kevinskt8-creator <kevinskt8@gmail.com>
2026-06-21 15:34:13 -0300

    Projeto calculadora Final

--- react/calculadora/src/compenents/button.jsx
+++ react/calculadora/src/compenents/button.jsx
@@ -1,7 +1,20 @@
 import React from "react";
 import './button.css'
 
-const Button = props =>
-    <button className="button">{props.label}</button>
+const Button = props => {
+    let classes = 'button'
+    classes += props.operation ? ' operation' : ''
+    classes += props.double ? ' double' : ''
+    classes += props.triple ? ' triple' : ''
+
+    return (
+        <button
+            onClick={e => props.click && props.click(props.label)}
+            className={classes}>
+            {props.label}
+        </button>
+    )
+}
 
 export default Button
+
