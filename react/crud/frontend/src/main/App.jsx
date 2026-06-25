fe21995e477519aa5665a0ba9a8c2cf8d3a004a9
kevinskt8-creator <kevinskt8@gmail.com>
2026-06-25 18:00:23 -0300

    Cadastro de uuarios 2

--- react/crud/frontend/src/main/App.jsx
+++ react/crud/frontend/src/main/App.jsx
@@ -1,16 +1,22 @@
-import './App.css'
-import React from 'react'
+import "bootstrap/dist/css/bootstrap.min.css";
+import "font-awesome/css/font-awesome.min.css";
+import "./App.css";
+import React from "react";
+import { BrowserRouter } from "react-router-dom";
 
-import Logo from '../components/template/Logo'
-import Nav from '../components/template/Nav'
-import Main from '../components/template/Main'
-import Footer from '../components/template/Footer'
+import Routes from "./Routes";
+import Logo from "../components/template/Logo";
+import Nav from "../components/template/Nav";
+import home from "../components/home/Home";
+import Footer from "../components/template/Footer";
 
-export default props =>
+export default (props) => (
+  <BrowserRouter>
     <div className="app">
-        <Logo />
-        <Nav />
-        <Main />
-        <Footer />
+      <Logo />
+      <Nav />
+      <Routes />
+      <Footer />
     </div>
-
+  </BrowserRouter>
+);
